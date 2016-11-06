8e0ccced1281bd6e25a4ceff830080d01c8350e0
Lucas Calazans <contat@lucascalazans.com.br>
2016-11-06 18:48:56 -0300

    Adding annotations to project

--- app/src/main/java/com/unifacs/eds2/adaptersample/MainActivity.java
+++ app/src/main/java/com/unifacs/eds2/adaptersample/MainActivity.java
@@ -1,13 +1,21 @@
 package com.unifacs.eds2.adaptersample;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    @ViewById(R.id.recycler_view)
+    RecyclerView rvList;
+
+    @AfterViews
+    protected void init() {
+
     }
+
 }
